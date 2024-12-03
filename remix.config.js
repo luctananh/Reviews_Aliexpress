e40d12af513f44dd9d5d8bcacb67b2d391a55122
luctananh <luctananh@example.com>
2024-12-03 10:00:12 +0700

    init

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     future: {
+//       v2_errorBoundary: true,
+//       v2_normalizeFormMethod: true,
+//       v2_meta: true,
+//       v2_headers: true,
+//       serverModuleFormat: 'esm',
+//     },
+//   };
+  
