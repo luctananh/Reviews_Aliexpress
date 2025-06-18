2f19649e857ac20b9f117931bff30da2f2c1c6bc
Lục Anh <120559330+luctananh@users.noreply.github.com>
2025-06-19 00:45:02 +0700

    Create remix.config.cjs

---
+++ remix.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build/server",
+  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ["**/.*"],
+};
+export default config;
