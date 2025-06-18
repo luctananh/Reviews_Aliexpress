7835dcb523c1c3f0edd884248824a0c0662231d6
Lục Anh <120559330+luctananh@users.noreply.github.com>
2025-06-19 00:12:34 +0700

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,13 @@
-// module.exports = {
-//     future: {
-//       v2_errorBoundary: true,
-//       v2_normalizeFormMethod: true,
-//       v2_meta: true,
-//       v2_headers: true,
-//       serverModuleFormat: 'esm',
-//     },
-//   };
-  
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  // Build server output vào đúng thư mục cần thiết cho deploy
+  serverBuildPath: "build/server/index.js",
+  // Nếu dùng Remix v2, bạn có thể bật các future flags này:
+  future: {
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_meta: true,
+    v2_headers: true,
+    serverModuleFormat: 'esm',
+  },
+};
