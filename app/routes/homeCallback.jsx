f22a2acfa728f476e1fe55e8667a6b23dd33f9d3
Lục Anh <120559330+luctananh@users.noreply.github.com>
2025-06-18 18:51:03 +0700

    Update homeCallback.jsx

--- app/routes/homeCallback.jsx
+++ app/routes/homeCallback.jsx
@@ -2,7 +2,7 @@ import { authenticator } from "../server/auth.server.js";
 
 export const loader = ({ request }) => {
   return authenticator.authenticate("auth0", request, {
-    successRedirect: "https://reviews-aliexpress.vercel.app/products",
+    successRedirect: "http://localhost:5173/products",
     failureRedirect: "",
   });
 };
