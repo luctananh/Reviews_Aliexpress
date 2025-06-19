0219cbb41a77ecb5cf64c9fb8a8e26d5ef71d281
project_3 <luctananh@gmail.com>
2025-06-19 22:38:14 +0700

    add supabase

---
+++ app/utlis/supabase.server.js
@@ -0,0 +1,26 @@
+const { createServerClient, serialize, parse } = require("@supabase/ssr");
+
+function createClient(request) {
+  const cookies = parse(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+
+  return createServerClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(key) {
+          return cookies[key];
+        },
+        set(key, value, options) {
+          headers.append("Set-Cookie", serialize(key, value, options));
+        },
+        remove(key, options) {
+          headers.append("Set-Cookie", serialize(key, "", options));
+        },
+      },
+    }
+  );
+}
+
+module.exports = { createClient };
