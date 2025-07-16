9e8b99cc64fd2f63e48605f6195e3087c7b256ef
project_3 <luctananh@gmail.com>
2025-07-16 08:56:08 +0700

    update auth0

--- app/routes/homeCallback.jsx
+++ app/routes/homeCallback.jsx
@@ -1,9 +1,8 @@
 import { authenticator } from "../server/auth.server.js";
 
 export const loader = ({ request }) => {
-  // Thêm dòng này để kiểm tra giá trị VERCEL_URL trong logs Vercel.
-  // Đảm bảo nó được đặt trước câu lệnh return.
-  console.log("VERCEL_URL from homeCallback:", process.env.VERCEL_URL);
+  // Log VERCEL_URL để kiểm tra giá trị thực tế trên Vercel Logs
+  console.log("VERCEL_URL from homeCallback:", process.env.VERCEL_URL); 
   return authenticator.authenticate("auth0", request, {
     successRedirect: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/products` : "/products",
     failureRedirect: "",
