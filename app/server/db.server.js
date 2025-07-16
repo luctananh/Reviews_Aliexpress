535273ba6b6428dd60f8c7c20545cdd8ced04a19
project_3 <luctananh@gmail.com>
2025-07-16 17:53:05 +0700

    PÍMA

--- app/server/db.server.js
+++ app/server/db.server.js
@@ -1,15 +1,39 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-let prisma;
+// let prisma;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.__db) {
-    global.__db = new PrismaClient();
-    global.__db.$connect();
-  }
-  prisma = global.__db;
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.__db) {
+//     global.__db = new PrismaClient();
+//     global.__db.$connect();
+//   }
+//   prisma = global.__db;
+// }
+
+// export { prisma };
+
+
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //change to reference a table in your schema
+  const val = await prisma.production.findMany({
+    take: 10,
+  });
+  console.log(val);
 }
 
-export { prisma };
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  process.exit(1);
+});
