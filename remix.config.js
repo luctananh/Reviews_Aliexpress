9c8e9e9c92adc8db5810a0a0a5faf07ebac92637
project_3 <luctananh@gmail.com>
2025-07-16 18:13:55 +0700

    ád

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   // Build server output vào đúng thư mục cần thiết cho deploy
-  serverBuildPath: "build/server/index.js",
+  serverBuildPath: "build/index.js", // Thay đổi đường dẫn này
   // Nếu dùng Remix v2, bạn có thể bật các future flags này:
   future: {
     v2_errorBoundary: true,
